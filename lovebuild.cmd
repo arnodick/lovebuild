57c4e7b60958dae3cbcd227248edbe7f131df27e
Ashley Pringle <ash.pringle@gmail.com>
2018-05-14 21:02:32 -0300

    Zip file now contains a folder rather than just the files

--- lovebuild.cmd
+++ lovebuild.cmd
@@ -1,6 +1,8 @@
+REM this script makes a folder containing your ready-to-play game, and a zip file ready for distribution, in whatever folder it is run
+REM the folder it creates will contain an exe file and all the required dlls, and the zip file will contain that same folder
 REM GAMENAME is both the name of the folder where your game's Lua code is, and the name that will be given to the directory and zip that this script outputs. avoid having spaces in it.
 SET GAMENAME=gamename
-REM LOVEDIR is a path to the directory where you have LOVE installed, where you can find love.exe and all the .dll files required when distributing a LOVE game.
+REM LOVEDIR is a path to the directory where you have LOVE installed, where you can find love.exe and all the dll files required when distributing a LOVE game.
 SET LOVEDIR="E:\Documents\Game Design\LOVE"
 REM PROJECTSDIR is a path to the directory where your LOVE projects are. the folder that has you game's Lua code in it should be the same as GAMENAME, and needs to be in this folder.
 SET "PROJECTSDIR=E:\Documents\Game Design\LOVE Projects\"
@@ -15,4 +17,4 @@ copy /b %LOVEDIR%\SDL2.dll %GAMENAME%\SDL2.dll
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory($env:PROJECTSDIR + $env:GAMENAME, $env:GAMENAME + '\' + $env:GAMENAME + '.love'); }"
 copy /b %LOVEDIR%\love.exe+%GAMENAME%\%GAMENAME%.love %GAMENAME%\%GAMENAME%.exe
 del %GAMENAME%\%GAMENAME%.love
-powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory($env:GAMENAME, $env:GAMENAME + '.zip'); }"
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory($env:GAMENAME, $env:GAMENAME + '.zip', 'Optimal', 'true'); }"
