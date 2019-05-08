38b57571c7318aef4f59c26ed3de6bbbe2f882ed
Ashley Pringle <ash.pringle@gmail.com>
2019-05-08 18:47:57 -0300

    Scripts now takes game name as a parameter, so a shortcut can be made for each game that you need built, while keeping the script unchanged.

--- lovebuild.cmd
+++ lovebuild.cmd
@@ -1,11 +1,11 @@
-REM this script makes a folder containing your ready-to-play game, and a zip file ready for distribution, in whatever folder it is run
-REM the folder it creates will contain an exe file and all the required dlls, and the zip file will contain that same folder
-REM GAMENAME is both the name of the folder where your game's Lua code is, and the name that will be given to the directory and zip that this script outputs. avoid having spaces in it.
-SET GAMENAME=gamename
-REM LOVEDIR is a path to the directory where you have LOVE installed, where you can find love.exe and all the dll files required when distributing a LOVE game.
+:: this script makes a folder containing your ready-to-play game, and a zip file ready for distribution, in whatever folder it is run
+:: the folder it creates will contain an exe file and all the required dlls, and the zip file will contain that same folder
+:: GAMENAME is both the name of the folder where your game's Lua code is, and the name that will be given to the directory and zip that this script outputs. avoid having spaces in it.
+SET GAMENAME=%1
+:: LOVEDIR is a path to the directory where you have LOVE installed, where you can find love.exe and all the dll files required when distributing a LOVE game.
 SET LOVEDIR="E:\Documents\Game Design\LOVE"
-REM PROJECTSDIR is a path to the directory where your LOVE projects are. the folder that has you game's Lua code in it should be the same as GAMENAME, and needs to be in this folder.
-SET "PROJECTSDIR=E:\Documents\Game Design\LOVE Projects\"
+:: PROJECTSDIR is a path to the directory where your LOVE projects are. the folder that has you game's Lua code in it should be the same as GAMENAME, and needs to be in this folder.
+SET "PROJECTSDIR=E:\Documents\Game Design\Projects\Love\"
 mkdir %GAMENAME%
 copy /b %LOVEDIR%\love.dll %GAMENAME%\love.dll
 copy /b %LOVEDIR%\lua51.dll %GAMENAME%\lua51.dll
