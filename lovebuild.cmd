83308b0a0a71741b7ca7c97137e4ae9c5fe5e921
Ashley Pringle <ash.pringle@gmail.com>
2021-10-03 23:37:25 -0300

    Added pause at end so window doesn't close automatically.

--- lovebuild.cmd
+++ lovebuild.cmd
@@ -3,8 +3,8 @@
 :: GAMENAME is both the name of the folder where your game's Lua code is, and the name that will be given to the directory and zip that this script outputs. avoid having spaces in it.
 SET GAMENAME=%1
 :: LOVEDIR is a path to the directory where you have LOVE installed, where you can find love.exe and all the dll files required when distributing a LOVE game.
-SET LOVEDIR="E:\Documents\Game Design\LOVE"
-:: PROJECTSDIR is a path to the directory where your LOVE projects are. the folder that has you game's Lua code in it should be the same as GAMENAME, and needs to be in this folder.
+SET LOVEDIR="E:\Documents\Game Design\Engines\LOVE"
+:: PROJECTSDIR is a path to the directory where your LOVE projects are. the folder that has your game's Lua code in it should be the same as GAMENAME, and needs to be in this folder.
 SET "PROJECTSDIR=E:\Documents\Game Design\Projects\Love\"
 mkdir %GAMENAME%
 copy /b %LOVEDIR%\love.dll %GAMENAME%\love.dll
@@ -17,4 +17,4 @@ copy /b %LOVEDIR%\SDL2.dll %GAMENAME%\SDL2.dll
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory($env:PROJECTSDIR + $env:GAMENAME, $env:GAMENAME + '\' + $env:GAMENAME + '.love'); }"
 copy /b %LOVEDIR%\love.exe+%GAMENAME%\%GAMENAME%.love %GAMENAME%\%GAMENAME%.exe
 del %GAMENAME%\%GAMENAME%.love
-powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory($env:GAMENAME, $env:GAMENAME + '.zip', 'Optimal', 'true'); }"
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory($env:GAMENAME, $env:GAMENAME + '.zip', 'Optimal', 'true'); Write-Host `n; Read-Host -Prompt 'Files copied and written.. YOU are free to go.... ('; }"
